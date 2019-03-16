b6cf81456d0031be5cd2a5d5ca7d4a73bc56722f
UnaHsu <sauuexux@gmail.com>
2019-03-16 15:14:14 +0800

    modify

--- 190316Class.py
+++ 190316Class.py
@@ -1,1 +1,15 @@
-grade = 60;
+grade = 59;
+if grade >= 60 and grade < 101:
+    print("及格");
+elif grade >= 0 and grade < 60:
+    print("不及格");
+else:
+    print("error");
+
+A=input("Please Input Here");
+if A=="Left" or A == "Lift Site":
+    print("You Won");
+elif A == "Right" or A == "Right Site":
+    print("You Lost");
+else:
+    print("Over");
