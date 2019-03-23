d7e1a10534053ad88f7aa447ba7589794e763185
Mobile User <mobile@una.local>
2019-03-23 13:40:08 +0800

    Modify

--- 190323Class.py
+++ 190323Class.py
@@ -3,7 +3,8 @@ import json
 
 token="EAAFDsWZAS5iYBAGZCo9ZCZCX5UlhVTra3KUafcBriKIf1OrkuAkWZBkBTbr4TqapRsp4VhtzZBKD0ZA2o2qX4mHpd8XHfrr0J8W7DfZBRFKhGUKVucMGFuFCQ5lbFzZBRghvSXUT5s28nTrbXCmOeyM0dpUheMiyfId8k5trv2BvP4LtZBynKt4O2H2nWd9qDOk00ZD";
 
-data = requests.get("https://graph.facebook.com/me/posts?access_token=" + token);
+
+data = requests.get("https://graph.facebook.com/me/posts?since=145160640&limit=100&access_token=" + token);
 jd = json.loads(data.text);
 
 #print(jd["data"][0]["id"]);
