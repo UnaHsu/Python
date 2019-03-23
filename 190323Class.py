197a9f05e34fca1e3abb63fe8e5ce9ea34f02f49
Mobile User <mobile@una.local>
2019-03-23 15:25:37 +0800

    Modify on una

--- 190323Class.py
+++ 190323Class.py
@@ -2,6 +2,7 @@ import requests
 import json
 import jieba
 
+corpus = [];
 token="EAAFDsWZAS5iYBAErQ61de1l1Ge9BzKhn1IOHvZCrHgaRllkfwyc5njZALievlY0YZBoi6NEiK4TmHnZAlNdLYGCPEb26jGeU4RIFsZAwK1rhZAiuyonLuia4FfDaY6kEdkKPS5U8ycWdTgGoUNwxPeZCuAnZCCZBZATBPOkUyfvTqBh1ZAQxssFfwljCm9ZB9Wh1yGIgZD";
 data = requests.get("https://graph.facebook.com/me/posts?since=145160640&limit=100&access_token=" + token);
 jd = json.loads(data.text);
@@ -14,6 +15,16 @@ while "paging" in jd:
     for post in jd["data"]:
         if "message" in post:
             #print(post["message"]);
-            print("，".join(jieba.cut(post["message"])));
+            corpus += jieba.cut(post["message"]);
+            #print("，".join(jieba.cut(post["message"])));
     data = requests.get(jd["paging"]["next"]);
     jd = json.loads(data.text);
+
+dic = {}    
+for ele in corpus:
+	if ele not in dic:
+    	dic[ele] = 1;
+	else:
+    	dic[ele] += 1;
+    
+print(dic)
