8191d8b4d13b61ac8c063c4f9a8331d0df1227a8
UnaHsu <sauuexux@gmail.com>
2019-03-23 13:43:55 +0800

    Modify

--- 190323Class.py
+++ 190323Class.py
@@ -1,14 +1,13 @@
 import requests
 import json
 
-token="EAAFDsWZAS5iYBAGZCo9ZCZCX5UlhVTra3KUafcBriKIf1OrkuAkWZBkBTbr4TqapRsp4VhtzZBKD0ZA2o2qX4mHpd8XHfrr0J8W7DfZBRFKhGUKVucMGFuFCQ5lbFzZBRghvSXUT5s28nTrbXCmOeyM0dpUheMiyfId8k5trv2BvP4LtZBynKt4O2H2nWd9qDOk00ZD";
-
-
+token="EAAFDsWZAS5iYBAErQ61de1l1Ge9BzKhn1IOHvZCrHgaRllkfwyc5njZALievlY0YZBoi6NEiK4TmHnZAlNdLYGCPEb26jGeU4RIFsZAwK1rhZAiuyonLuia4FfDaY6kEdkKPS5U8ycWdTgGoUNwxPeZCuAnZCCZBZATBPOkUyfvTqBh1ZAQxssFfwljCm9ZB9Wh1yGIgZD";
 data = requests.get("https://graph.facebook.com/me/posts?since=145160640&limit=100&access_token=" + token);
 jd = json.loads(data.text);
 
+print(jd);
 #print(jd["data"][0]["id"]);
 
-for post in jd["data"]:    
+for post in jd["data"]:
     if "message" in post:
         print(post["message"]);
